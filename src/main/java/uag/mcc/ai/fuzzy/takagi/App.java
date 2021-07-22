c28b54905ac414099309add90d9ad2c7df9997f7
Fabian Rivera <fabian.riveram@gmail.com>
2021-07-21 21:33:35 -0500

    initial commit

---
+++ src/main/java/uag/mcc/ai/fuzzy/takagi/App.java
@@ -0,0 +1,12 @@
+package uag.mcc.ai.fuzzy.takagi;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+        log.info("starting Takagi Sugeno Network Sample");
+    }
+
+}
